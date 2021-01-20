b47de5e39a5095d5f0b5ecd0a6f7d0290436cb4e
shusterboris <shuster.boris@gmail.com>
2021-01-20 10:42:50 +0200

    Преобразование номера статуса в строку и строки в номер (статические методы)

--- src/main/java/enums/ProposalStatus.java
+++ src/main/java/enums/ProposalStatus.java
@@ -6,4 +6,21 @@ public enum ProposalStatus {
 	public String getMessageKey() {
 		return getClass().getSimpleName() + "." + name();
 	}
+	
+	public static String getMessageKeyByNumber(int key) {
+		for(ProposalStatus status : ProposalStatus.values()) {
+			if (status.ordinal() == key)
+				return status.getMessageKey();
+		}
+		return "ProposalStatus.INIT";
+	}
+	
+	public static int getOrdinalByMessage(String key) {
+		for(ProposalStatus status : ProposalStatus.values()) {
+			if (key.equals(status.getMessageKey()))
+				return status.ordinal();
+		}
+		return 0;
+	}
+	
 }
