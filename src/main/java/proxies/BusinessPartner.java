44ce17a2090a58f6ef1dd4855ae1b8351166db65
shusterboris <shuster.boris@gmail.com>
2021-02-01 17:59:57 +0200

    Исправлена ошибка в конструкторе BusinessPartner.java

--- src/main/java/proxies/BusinessPartner.java
+++ src/main/java/proxies/BusinessPartner.java
@@ -5,80 +5,76 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class BusinessPartner implements Serializable {
-private static final long serialVersionUID = -7523924559237866260L;
+	private static final long serialVersionUID = -7523924559237866260L;
 	private long id;
-private String name;
-private String fullName;
-private String phone;
-private boolean supplier = false;
-private List<Contact> contacts;
-private List<Store> stores;
-private Double raiting;  //оценка поставщика
-public Double getRaiting() {
-	return raiting;
-}
-public void setRaiting(Double raiting) {
-	this.raiting = raiting;
-}
-public void setContacts(List<Contact> contacts) {
-	this.contacts = contacts;
-}
-public void setStores(List<Store> stores) {
-	this.stores = stores;
-}
-public String getName() {
-	return name;
-}
-public void setName(String name) {
-	this.name = name;
-}
-public String getFullName() {
-	return fullName;
-}
-public void setFullName(String fullName) {
-	this.fullName = fullName;
-}
-public String getPhone() {
-	return phone;
-}
-public void setPhone(String phone) {
-	this.phone = phone;
-}
-public List<Contact> getContacts() {
-	return contacts;
-}
-public void setContacts(ArrayList<Contact> contacts) {
-	this.contacts = contacts;
-}
-public List<Store> getStores() {
-	return stores;
-}
+	private String name;
+	private String fullName;
+	private String phone;
+	private boolean supplier = false;
+	private List<Contact> contacts;
+	private List<Store> stores;
+	private Double raiting; // оценка поставщика
 
-public void setStores(ArrayList<Store> stores) {
-	this.stores = stores;
-}
+	public Double getRaiting() {
+		return raiting;
+	}
 
+	public void setRaiting(Double raiting) {
+		this.raiting = raiting;
+	}
 
+	public void setContacts(List<Contact> contacts) {
+		this.contacts = contacts;
+	}
 
+	public void setStores(List<Store> stores) {
+		this.stores = stores;
+	}
 
-public boolean isSupplier() {
-	return supplier;
-}
-public void setSupplier(boolean supplier) {
-	this.supplier = supplier;
-}
+	public String getName() {
+		return name;
+	}
 
-	/*
-	 * public BusinessPartner(Long id, String name, Address address, Long
-	 * headQuatersId) { super(id, name, address, headQuatersId); this.headQuatersId
-	 * = (long) 0; }
-	 */
-	/*
-	 * public String toString() { StringBuilder sb = new StringBuilder();
-	 * sb.append(getName() !=null ? getName() : getFullName());
-	 * sb.append(getAddress() != null ? getAddress().getSettlement() : ""); return
-	 * sb.toString(); }
-	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getFullName() {
+		return fullName;
+	}
+
+	public void setFullName(String fullName) {
+		this.fullName = fullName;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public List<Contact> getContacts() {
+		return contacts;
+	}
+
+	public void setContacts(ArrayList<Contact> contacts) {
+		this.contacts = contacts;
+	}
+
+	public List<Store> getStores() {
+		return stores;
+	}
+
+	public boolean isSupplier() {
+		return supplier;
+	}
+
+	public void setSupplier(boolean supplier) {
+		this.supplier = supplier;
+	}
+
 	public String toString() {
 		StringBuilder sb = new StringBuilder();
 		sb.append(getFullName());
