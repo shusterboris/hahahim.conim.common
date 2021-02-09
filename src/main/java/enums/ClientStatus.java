f25fe70e7c18c3a732d38a6524c9033935e720a5
shusterboris <shuster.boris@gmail.com>
2021-02-09 11:58:01 +0200

    Упрощены статусы клиента

--- src/main/java/enums/ClientStatus.java
+++ src/main/java/enums/ClientStatus.java
@@ -1,11 +1,9 @@
 package enums;
 
 public enum ClientStatus {
-	NEW, CANDIDATE, ACTIVE, FROZEN, SUSPENDED, ARCHIVE;
+	NEW, ACTIVE, FROZEN, SUSPENDED, ARCHIVE;
 
 	public String getMessageKey() {
 		return getClass().getSimpleName() + "." + name();
 	}
-	// POTENTIAL - это новый пользователь, возможно, свежезарегистрированный
-	// CANDIDATE - проверенный модератором, например, может принять участие в 3-4 акциях
 }
